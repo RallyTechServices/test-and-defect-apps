c12e9d3ae13d87d31c090f81e8c7915c1514b7b4
John Martin <curmudgeon+git@rallydev.com>
2014-09-11 19:24:31 -0700

    show fields defined in config apps

--- defect-trend-by-date/src/javascript/_defect_calculator.js
+++ defect-trend-by-date/src/javascript/_defect_calculator.js
@@ -1,66 +1,115 @@
 Ext.define("Rally.TechnicalServices.burndown.DefectTrendCalculator", {
     extend: "Rally.data.lookback.calculator.TimeSeriesCalculator",
 
+    allowedValues: [],
+    includeCreatedTotal: false,
+    
     config: {
         closedStateNames: ['Closed']
     },
     
     getDerivedFieldsOnInput: function () {
         var completedStates = this.config.closedStateNames
+        var oid_cutline = 20631303804;
+        var allowed_values = this.allowedValues || [];
         
-        return [
-            {
+        if ( typeof allowed_values === 'string' ) {
+            allowed_values = allowed_values.split(/,/);
+        }
+        var derived_fields = [];
+        if ( this.includeCreatedTotal ) {
+            derived_fields.push({
+                "as": "ExistingDefect",
+                "f": function (snapshot) {  
+                    if (snapshot.ObjectID > oid_cutline) {
+                        return true;
+                    } 
+                    return false;
+                }
+            });
+        }
+        if ( allowed_values.length === 0 ) {
+            derived_fields.push({
                 "as": "ActiveDefect",
                 "f": function (snapshot) {
                     var state = snapshot.State;
                     
-                    if (Ext.Array.indexOf(completedStates,state) == -1) {
+                    if (snapshot.ObjectID > 20631303804 && Ext.Array.indexOf(completedStates,state) == -1) {
                         return true;
                     }
                     return false;
                 }
-            },
-            {
+            });
+            derived_fields.push({
                 "as": "ClosedDefect",
                 "f": function (snapshot) {
                     var state = snapshot.State;
                     
-                    if (Ext.Array.indexOf(completedStates,state) > -1) {
+                    if (snapshot.ObjectID > 20631303804 && Ext.Array.indexOf(completedStates,state) > -1) {
                         return true;
                     }
                     return false;
                 }
-            },
-            {
-                "as": "ExistingDefect",
-                "f": function (snapshot) {  
-                    return true;
-                }
-            }
-        ];
+            });
+        } else {
+            Ext.Array.each(allowed_values, function(allowed_value){
+                derived_fields.push({
+                    "as": allowed_value,
+                    "f": function (snapshot) {
+                        var state = snapshot.State;
+                        
+                        if (snapshot.ObjectID > 20631303804 && state == allowed_value ) {
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+            });
+        }
+        
+        return derived_fields;
     },
 
     getMetrics: function () {
-        return [
-            {
+        var allowed_values = this.allowedValues || [];
+        if ( typeof allowed_values === 'string' ) {
+            allowed_values = allowed_values.split(/,/);
+        }
+        var metrics = [];
+        if ( this.includeCreatedTotal ) {
+            metrics.push({
                 "filterField": "ExistingDefect",
                 'as':'Created',
                 'f':'filteredCount',
                 'filterValues':[true]
-            },
-            {
+            });
+        }
+        
+        if ( allowed_values.length === 0 ) {
+            metrics.push({
                 "filterField": "ActiveDefect",
                 "as": "Open",
                 "f": "filteredCount",
                 "filterValues": [true]
-            },
-            {
+            });
+            metrics.push({
                 "filterField": "ClosedDefect",
                 "as": "Closed",
                 "f": "filteredCount",
                 "filterValues": [true]
-            }
-        ];
+            });
+        } else {
+            Ext.Array.each(allowed_values, function(allowed_value){
+                metrics.push({
+                    "filterField": allowed_value,
+                    "as": allowed_value,
+                    "f": "filteredCount",
+                    "filterValues": [true]
+                });
+            });
+        }
+        
+        return metrics;
     },
 
     getSummaryMetricsConfig: function () {
@@ -75,7 +124,6 @@ Ext.define("Rally.TechnicalServices.burndown.DefectTrendCalculator", {
         var chartData = this.callParent(arguments);
 
         this._formatCategories(chartData);
-        console.log(chartData);
         return chartData;
     },
     
--- defect-trend-by-date/src/javascript/app.js
+++ defect-trend-by-date/src/javascript/app.js
@@ -9,6 +9,13 @@ Ext.define('CustomApp', {
         {xtype:'tsinfolink'}
     ],
     launch: function() {
+        if (this.isExternal()){
+            this.showSettings(this.config);
+        } else {
+            this.onSettingsUpdate(this.getSettings());  
+        }  
+    },
+    _preProcess: function() {
         this.down('#selector_box').add({
             xtype:'rallydatefield',
             fieldLabel: 'Start Date:',
@@ -21,10 +28,49 @@ Ext.define('CustomApp', {
                 change: function(box) {
                     var start_date = box.getSubmitValue();
                     this.logger.log("Start Date:",start_date);
-                    this._makeChart(this.down('#display_box'),start_date);
+
+                    this._getAllowedValues().then({
+                        scope: this,
+                        success:function(results){
+                            this._makeChart(this.down('#display_box'),start_date,results);
+                        },
+                        failure:function(message){
+                            this.down('#display_box').add({xtype:'container',html:'message'});
+                        }
+                    });
+                    
                 }
             }
         });
+    },
+    _getAllowedValues:function(){
+        var deferred = Ext.create('Deft.Deferred');
+        var type_path = 'Defect';
+        var group_by_field = 'State';
+        
+        var allowed_values = [];
+        
+        Rally.data.ModelFactory.getModel({
+            type: type_path,
+            success: function(model){
+                var field = model.getField(group_by_field);
+                var attribute_definition = field.attributeDefinition;
+                if ( attribute_definition && attribute_definition.AttributeType == "BOOLEAN" ) {
+                    deferred.resolve([true,false]);
+                } else {
+                    field.getAllowedValueStore().load({
+                        callback: function(values,operation,success) {
+                            Ext.Array.each(values, function(value){
+                                allowed_values.push(value.get('StringValue'));
+                            });
+                            deferred.resolve(allowed_values);
+                        }
+                    });
+                }
+            },
+            scope: this
+        });
+        return deferred.promise;
     },
     _getUserTimeZone: function() {
         var tz = this.getContext().getUser().UserProfile.TimeZone;
@@ -33,10 +79,12 @@ Ext.define('CustomApp', {
         }
         return tz;
     },
-    _makeChart: function(display_box,start_date) {
-        this.logger.log("_makeChart",start_date);
+    _makeChart: function(display_box,start_date,allowed_values) {
+        this.logger.log("_makeChart",start_date,allowed_values);
         start_date = Rally.util.DateTime.fromIsoString(start_date);
-                
+        
+        var show_states = this.getSetting('allowed_values') || allowed_values;
+        
         display_box.removeAll();
         display_box.add({
             xtype:'rallychart',
@@ -54,6 +102,8 @@ Ext.define('CustomApp', {
                 timeZone: this._getUserTimeZone(),
                 startDate: start_date,
                 granularity: 'day',
+                allowedValues: show_states,
+                includeCreatedTotal: this.getSetting('show_created_count') || false,
                 workDays: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
             },
             sort: {
@@ -80,5 +130,90 @@ Ext.define('CustomApp', {
                 }
             }
         });
+    },
+    isExternal: function(){
+      return typeof(this.getAppId()) == 'undefined';
+    },
+    
+    _addCountToChoices: function(store){
+        store.add({name:'Count',value:'Count',fieldDefinition:{}});
+    },
+        
+    /********************************************
+    /* Overrides for App class
+    /*
+    /********************************************/
+    //getSettingsFields:  Override for App    
+    getSettingsFields: function() {
+        var me = this;
+        
+        var allowed_values = this.getSetting('allowed_values');
+        this.logger.log("getting fields", allowed_values, typeof allowed_values);
+        
+        if ( typeof allowed_values === 'string' ) {
+            allowed_values = allowed_values.split(/,/);
+        }
+        
+        return [   {
+            name: 'allowed_values',
+            xtype: 'rallyfieldvaluecombobox',
+            fieldLabel: 'Show States:',
+            field: 'State',
+            model: 'Defect',
+            labelWidth: 75,
+            width: 400,
+            margin: 10,
+            autoExpand: true,
+            alwaysExpanded: true,
+            multiSelect: true,
+            readyEvent: 'ready'
+        },
+        {
+            name: 'show_created_count',
+            xtype:'rallycheckboxfield',
+            fieldLabel: 'Show Total:',
+            labelWidth: 75,
+            margin: 10,
+            readyEvent: 'ready'
+            
+        }];
+    },
+    //showSettings:  Override to add showing when external + scrolling
+    showSettings: function(options) {
+        this.logger.log("showSettings",options);
+        this._appSettings = Ext.create('Rally.app.AppSettings', Ext.apply({
+            fields: this.getSettingsFields(),
+            settings: this.getSettings(),
+            defaultSettings: this.getDefaultSettings(),
+            context: this.getContext(),
+            settingsScope: this.settingsScope
+        }, options));
+
+        this._appSettings.on('cancel', this._hideSettings, this);
+        this._appSettings.on('save', this._onSettingsSaved, this);
+        
+        if (this.isExternal()){
+            if (this.down('#display_box').getComponent(this._appSettings.id)==undefined){
+                this.down('#display_box').add(this._appSettings);
+            }
+        } else {
+            this.hide();
+            this.up().add(this._appSettings);
+        }
+        return this._appSettings;
+    },
+    _onSettingsSaved: function(settings){
+        this.logger.log('_onSettingsSaved',settings);
+        Ext.apply(this.settings, settings);
+        this._hideSettings();
+        this.onSettingsUpdate(settings);
+    },
+    //onSettingsUpdate:  Override
+    onSettingsUpdate: function (settings){
+        //Build and save column settings...this means that we need to get the display names and multi-list
+        this.logger.log('onSettingsUpdate',settings);
+        
+        var type = this.getSetting('type');
+        this._preProcess();
     }
 });
